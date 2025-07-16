75c64d5b92056ae8c7e797ae69c4967e927c2b16
Nikhil Verma <its-nikhil@hotmail.com>
2025-07-17 01:21:26 +0530

    final commit

--- backend/app.py
+++ backend/app.py
@@ -91,3 +91,8 @@ async def recommend_fashion(file: UploadFile = File(...)):
         return JSONResponse(content={"recommendations": recommended_images})
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+# Run app: uvicorn fashion_app:app --reload
+if __name__ == "__main__":
+    uvicorn.run("fashion_app:app", host="0.0.0.0", port=8000, reload=True)
